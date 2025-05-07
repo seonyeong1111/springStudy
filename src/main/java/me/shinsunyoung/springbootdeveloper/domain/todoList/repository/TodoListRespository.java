2cc56c5fe8f4d49e41166c74dc11fae116586835
Seonyeong Park <bagseonyeong452@gmail.com>
2025-05-07 12:34:13 +0900

    todo야 사라져

--- src/main/java/me/shinsunyoung/springbootdeveloper/domain/todoList/repository/TodoListRespository.java
+++
@@ -1,9 +0,0 @@
-package me.shinsunyoung.springbootdeveloper.domain.todoList.repository;
-
-import me.shinsunyoung.springbootdeveloper.domain.todoList.domain.TodoList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoListRespository extends JpaRepository<TodoList, Long> {
-}
